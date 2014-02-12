dc804079765915dac7595c44ddd50ee78355fd2d
Lucas Penney <lucaspenney@gmail.com>
2014-02-11 21:29:23 -0800

    Deck loaded through DeckManager, cards fade in
    Removed card.js (keeping cards as Kinetic objects on layers, also
    removed Builder.cards array). Some changes to the builder structure to
    keep event-driven.

--- js/builder.js
+++ js/builder.js
@@ -1,42 +1,52 @@
-var Builder = new DeckBuilder();
+var Builder = null;
 
-function DeckBuilder() {
-	this.deckId = null;
-	this.lastSaved = null;
-	this.cards = [];
-}
-
-$(window).load(function() {
-	Builder.stage = new Kinetic.Stage({
-		container: 'container',
-		width: 600 * 2,
-		height: 400 * 2
-	});
-
-	Builder.mainBoard = new Kinetic.Layer();
-	Builder.sideBoard = new Kinetic.Layer();
-
-	Builder.stage.add(Builder.mainBoard);
-	Builder.stage.add(Builder.sideBoard);
-
-	createCard(4913);
-
-
-
+$(document).ready(function() {
+	Builder = new DeckBuilder();
 	Builder.mainBoard.draw();
 
 	loop();
 });
 
+
+function DeckBuilder() {
+	this.lastSaved = null;
+	this.deckManager = new DeckManager();
+	this.deckManager.loadDeck(8);
+
+	this.stage = new Kinetic.Stage({
+		container: 'container',
+		width: 600 * 2,
+		height: 400 * 2
+	});
+
+	this.mainBoard = new Kinetic.Layer();
+	this.sideBoard = new Kinetic.Layer();
+
+	this.stage.add(this.mainBoard);
+	this.stage.add(this.sideBoard);
+
+	this.mainBoard.on('mouseover', function(evt) {
+
+	});
+}
+
+DeckBuilder.prototype.onLoad = function() {
+	Builder.mainBoard.getChildren().each(function(node, index) {
+		node.scale({
+			x: 0.3,
+			y: 0.3
+		});
+		node.position({
+			x: index * 20,
+			y: index * 1
+		});
+		node.animateFadeIn.play();
+	});
+};
+
 function loop() {
 	requestAnimationFrame(loop);
 	Builder.mainBoard.draw();
 
-	Builder.mainBoard.getChildren().each(function(node, index) {
-		node.scale({
-			x: 0.5,
-			y: 0.5
-		});
-		node.animateTap.play();
-	});
+
 }
