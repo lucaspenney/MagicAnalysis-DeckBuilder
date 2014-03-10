a2b42631814787ccb58bf592b6074478ff11a089
Lucas Penney <lucaspenney@gmail.com>
2014-03-10 14:42:34 -0700

    Changes to allow drawing graphs with builder

--- js/grapher.js
+++ js/grapher.js
@@ -1,4 +1,25 @@
 function Grapher() {
-	//Let's make some graphs. 
 
 }
+
+Grapher.prototype.load = function() {
+    //Let's make some graphs. 
+    //Graphs created by jqPlot
+    var costGraph = $.jqplot('graphs', [
+        [3, 7, 9, 1, 4, 6, 8]
+    ], {
+        title: 'Mana Cost Distribution',
+        axesDefaults: {
+            labelRenderer: $.jqplot.CanvasAxisLabelRenderer
+        },
+        axes: {
+            xaxis: {
+                label: "Mana Cost",
+            },
+            yaxis: {
+                label: "Cards"
+            }
+        }
+    });
+
+};
